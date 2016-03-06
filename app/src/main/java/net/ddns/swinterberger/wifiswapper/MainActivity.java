d67399e0ac3d1d35fcf4111c5106b3fad4dcdd76
Stefan Winterberger <stefanwinterb88@hotmail.com>
2016-03-06 19:44:23 +0100

    Can now presistent safe the current values in preferences.

--- app/src/main/java/net/ddns/swinterberger/wifiswapper/MainActivity.java
+++ app/src/main/java/net/ddns/swinterberger/wifiswapper/MainActivity.java
@@ -1,17 +1,21 @@
 package net.ddns.swinterberger.wifiswapper;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import net.ddns.swinterberger.wifiswapper.eventhandler.MarginSeekbarEventhandler;
-import net.ddns.swinterberger.wifiswapper.eventhandler.TresholdSeekbarEventhandler;
+import net.ddns.swinterberger.wifiswapper.eventhandler.ThresholdSeekbarEventhandler;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String MARGIN_PREFERENCE_NAME = "MarginPreference";
+    private final String THRESHOLD_PREFERENCE_NAME = "ThresholdPreference";
     private TextView debugInfos;
-    private SeekBar tresholdSeekbar;
+    private SeekBar thresholdSeekbar;
     private SeekBar marginSeekbar;
     private TextView tresholdValue;
     private TextView marginValue;
@@ -22,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupViewElements();
-
+        loadPreferences();
+        startSwapperService();
     }
 
     private void setupViewElements() {
@@ -31,10 +36,28 @@ public class MainActivity extends AppCompatActivity {
         tresholdValue = (TextView) findViewById(R.id.tvThresoldValue);
         marginValue = (TextView) findViewById(R.id.tvMarginValue);
 
-        tresholdSeekbar = (SeekBar) findViewById(R.id.seekBar_Treshold);
-        tresholdSeekbar.setOnSeekBarChangeListener(new TresholdSeekbarEventhandler(this.tresholdValue));
+        thresholdSeekbar = (SeekBar) findViewById(R.id.seekBar_Treshold);
+        ThresholdSeekbarEventhandler thresholdSeekbarEventhandler = new ThresholdSeekbarEventhandler(this);
+        thresholdSeekbarEventhandler.setThresholdValue(this.tresholdValue);
+        thresholdSeekbar.setOnSeekBarChangeListener(thresholdSeekbarEventhandler);
+        thresholdSeekbarEventhandler.setTHRESOLD_PREFERENCE_NAME(this.THRESHOLD_PREFERENCE_NAME);
 
         marginSeekbar = (SeekBar) findViewById(R.id.seekBar_Margin);
-        marginSeekbar.setOnSeekBarChangeListener(new MarginSeekbarEventhandler(this.marginValue));
+        MarginSeekbarEventhandler marginSeekbarEventhandler = new MarginSeekbarEventhandler(this);
+        marginSeekbarEventhandler.setMarginValue(this.marginValue);
+        marginSeekbar.setOnSeekBarChangeListener(marginSeekbarEventhandler);
+        marginSeekbarEventhandler.setMARGIN_PREFERENCE_NAME(this.MARGIN_PREFERENCE_NAME);
+    }
+
+    private void loadPreferences() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        int currentMarginValue = preferences.getInt(this.MARGIN_PREFERENCE_NAME, 0);
+        int currentThresholdValue = preferences.getInt(this.THRESHOLD_PREFERENCE_NAME, 0);
+
+        this.marginSeekbar.setProgress(currentMarginValue);
+        this.thresholdSeekbar.setProgress(currentThresholdValue);
+    }
+
+    private void startSwapperService() {
     }
 }
--- app/src/main/java/net/ddns/swinterberger/wifiswapper/eventhandler/ThresholdSeekbarEventhandler.java
+++ app/src/main/java/net/ddns/swinterberger/wifiswapper/eventhandler/ThresholdSeekbarEventhandler.java
@@ -4,34 +4,32 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 /**
  * Created by Stefan on 05.03.2016.
  */
-public class MarginSeekbarEventhandler implements OnSeekBarChangeListener {
+public class ThresholdSeekbarEventhandler implements SeekBar.OnSeekBarChangeListener {
 
     private final Context context;
-    private String MARGIN_PREFERENCE_NAME;
-    private TextView marginValue;
+    private String THRESOLD_PREFERENCE_NAME;
+    private TextView thresholdValue;
 
-    public MarginSeekbarEventhandler(Context context) {
+    public ThresholdSeekbarEventhandler(Context context) {
         this.context = context;
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
         int seekBarValue = seekBar.getProgress();
 
-        if (marginValue != null) {
-            marginValue.setText(String.valueOf(seekBarValue / 10));
+        if (thresholdValue != null) {
+            thresholdValue.setText(String.valueOf(seekBarValue / 10));
         }
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this.context);
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putInt(this.MARGIN_PREFERENCE_NAME, seekBarValue);
+        editor.putInt(this.THRESOLD_PREFERENCE_NAME, seekBarValue);
         editor.apply();
     }
 
@@ -45,19 +43,19 @@ public class MarginSeekbarEventhandler implements OnSeekBarChangeListener {
 
     }
 
-    public TextView getMarginValue() {
-        return marginValue;
+    public TextView getThresholdValue() {
+        return thresholdValue;
     }
 
-    public void setMarginValue(TextView marginValue) {
-        this.marginValue = marginValue;
+    public void setThresholdValue(TextView thresholdValue) {
+        this.thresholdValue = thresholdValue;
     }
 
-    public String getMARGIN_PREFERENCE_NAME() {
-        return MARGIN_PREFERENCE_NAME;
+    public String getTHRESOLD_PREFERENCE_NAME() {
+        return THRESOLD_PREFERENCE_NAME;
     }
 
-    public void setMARGIN_PREFERENCE_NAME(String MARGIN_PREFERENCE_NAME) {
-        this.MARGIN_PREFERENCE_NAME = MARGIN_PREFERENCE_NAME;
+    public void setTHRESOLD_PREFERENCE_NAME(String THRESOLD_PREFERENCE_NAME) {
+        this.THRESOLD_PREFERENCE_NAME = THRESOLD_PREFERENCE_NAME;
     }
 }
